456fbe696253a46e1a09036bdb0ed80d81627a1e
Максим Тамошкин <tamoshkin.m@yandex.ru>
2023-09-13 22:13:23 +0700

    добавил коммент для проверки работоспособности

---
+++ CodeBlogFitness.BL/Controller/UserController.cs
@@ -0,0 +1,14 @@
+﻿using CodeBlogFitness.BL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogFitness.BL.Controller
+{
+    internal class UserController
+    {
+        public User User { get; }
+    }
+}
